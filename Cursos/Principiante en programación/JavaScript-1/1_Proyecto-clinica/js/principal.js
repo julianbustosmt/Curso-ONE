c45ec88e807399181fbf946f678705c4999b79d6
julianbustosmt <julian.bustos.mt@correo.usa.edu.co>
2022-04-25 15:55:26 -0500

    Eventos, formularios y creando elementos

--- Cursos/Principiante en programación/JavaScript-1/1_Proyecto-clinica/js/principal.js
+++ Cursos/Principiante en programación/JavaScript-1/1_Proyecto-clinica/js/principal.js
@@ -1,5 +1,42 @@
+const btnAdicionar = document.querySelector("#adicionar-paciente")
+
+btnAdicionar.addEventListener("click", function(event){
+    event.preventDefault()
+    const form = document.querySelector("#form-adicionar")
+    
+    const nombre = form.nombre.value;
+    const peso = form.peso.value;
+    const altura = form.altura.value;
+    const gordura = form.gordura.value;
+
+    // <tr></tr>
+    const pacienteTr = document.createElement("tr")
+    // <td></td>
+    const nombreTd = document.createElement("td")
+    const alturaTd = document.createElement("td")
+    const pesoTd = document.createElement("td")
+    const gorduraTd = document.createElement("td")
+
+    nombreTd.textContent = nombre
+    alturaTd.textContent = altura
+    pesoTd.textContent = peso
+    gorduraTd.textContent = gordura
+
+    pacienteTr.appendChild(nombreTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+
+    const tabla = document.querySelector("#tabla-pacientes")
+    tabla.appendChild(pacienteTr)
+
+    
+})
+
+
 const pacientes = document.querySelectorAll(".paciente")
 console.log(pacientes)
+
 
 for (let i = 0; i < pacientes.length; i++) {
     let paciente = pacientes[i]
