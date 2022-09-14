76d0a7aa9544a62095e9b20d80a990631fbcd444
Julian Esteban Bustos <julianestebanbustos@gmail.com>
2022-09-14 09:56:13 -0500

    Agregando metodos

--- Cursos/Front-end/JavaScript/2_Herencia e interfaces en Orientación a objetos/index.js
+++ Cursos/Front-end/JavaScript/2_Herencia e interfaces en Orientación a objetos/index.js
@@ -6,26 +6,74 @@ const saldoCuenta = 1000
 
 //Creacion de clase
 class cliente {
-    nombreCliente
-    dniCliente
-    numeroCuenta
-    saldoCuenta
+    nombreCliente;
+    dniCliente;
+    rutCliente;
 }
 
-const cliente1 = new cliente()
+class cuentaCorriente {
+    numero;
+    #saldo; // # Convierte el atributo en privado
+    //saldo; 
+    agencia;
 
-cliente1.nombreCliente = "Jose"
-cliente1.dniCliente = "1111111"
-cliente1.numeroCuenta = "12312313"
-cliente1.saldoCuenta = 12000
+    constructor(){
+        this.numero = ""
+        this.#saldo = 0;
+        this.agencia = ""
+    }
 
-console.log(cliente1)
+    //Metodos 
+    deposito = (valor) =>{
+        if(valor > 0){
+            this.#saldo += valor;
+        }
+        return this.#saldo
+    }
 
-const cliente2 = new cliente()
+    retiro = (valor) =>{
+        if(valor <= this.#saldo){
+            this.#saldo -= valor;
+        }else{
+            console.log("Saldo insuficiente")
+        }
+        return this.#saldo
+    }
 
-cliente2.nombreCliente = "Pepe"
-cliente2.dniCliente = "1111222111"
-cliente2.numeroCuenta = "1213123312313"
-cliente2.saldoCuenta = 123000
+    verSaldo = () =>{
+        return this.#saldo
+    }
+}
 
-console.log(cliente2)
+// Istancia o objeto de la clase 
+const cliente1 = new cliente();
+
+// Asignacion de valores a los atributos de la clase
+cliente1.nombreCliente = "Jose";
+cliente1.dniCliente = "1111111";
+cliente1.rutCliente = "1112221111";
+
+const cuentaCorriente1 = new cuentaCorriente();
+cuentaCorriente1.numero = "12312313";
+/* cuentaCorriente1.#saldo = 12000; */
+cuentaCorriente1.agencia = "elPepe";
+
+let saldo = cuentaCorriente1.verSaldo();
+console.log(`El saldo actual es ${saldo}`);
+cuentaCorriente1.deposito(1000);
+
+saldo = cuentaCorriente1.verSaldo();
+console.log(`El saldo actual es ${saldo}`);
+
+cuentaCorriente1.retiro(15000);
+
+/* const cliente2 = new cliente();
+
+cliente2.nombreCliente = "Pepe";
+cliente2.dniCliente = "1111222111";
+cliente2.rutCliente = "33132131";
+
+const cuentaCorriente2 = new cuentaCorriente();
+cuentaCorriente2.numero = "1213123312313";
+cuentaCorriente2.saldo = 123000;
+cuentaCorriente2.agencia = "elPepe2"; */
