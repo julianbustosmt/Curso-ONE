cffb994f845d5706bcf6ff0b68856abd41a53127
Julian Esteban Bustos <julianestebanbustos@gmail.com>
2022-09-20 11:12:48 -0500

    Agregar una tarea a la lista

--- Cursos/Front-end/JavaScript/JS-Web/1_Manipulacion del DOOM/script.js
+++ Cursos/Front-end/JavaScript/JS-Web/1_Manipulacion del DOOM/script.js
@@ -1,9 +1,26 @@
-const btn = document.querySelector('[data-form-btn]')
-const input = document.querySelector('[data-form-input]')
+const btn = document.querySelector('[data-form-btn]');
+
 
 const creteTask = (evento) =>{
-    evento.preventDefault()
-    console.log(input.value)
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const task = document.createElement('li');
+    const list = document.querySelector('[data-list]');
+    task.classList.add("card")
+
+    value = input.value;
+
+    const content = 
+        `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`
+    
+    task.innerHTML = content;
+
+    list.appendChild(task);
+    input.value = "";
 }
 
 btn.addEventListener("click",creteTask)
