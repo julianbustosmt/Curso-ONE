742e6cfc85ab3d72f9b027d2c4f6ec1ae3e141a8
Julian Esteban Bustos <julianestebanbustos@gmail.com>
2022-09-21 11:57:58 -0500

    Eliminar una tarea

--- Cursos/Front-end/JavaScript/JS-Web/1_Manipulacion del DOOM/script.js
+++ Cursos/Front-end/JavaScript/JS-Web/1_Manipulacion del DOOM/script.js
@@ -1,48 +1,29 @@
-(() => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
-    const btn = document.querySelector('[data-form-btn]');
+const btn = document.querySelector('[data-form-btn]');
 
-    const creteTask = (evento) => {
-        evento.preventDefault();
-        const input = document.querySelector('[data-form-input]');
-        const task = document.createElement('li');
-        const list = document.querySelector('[data-list]');
-        task.classList.add("card");
+const creteTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const task = document.createElement('li');
+    const list = document.querySelector('[data-list]');
+    task.classList.add("card");
 
-        value = input.value;
-        const taskContent = document.createElement('div');
-        taskContent.appendChild(checkComplete());
-        const titleTask = document.createElement('span');
-        titleTask.classList.add('task');
-        titleTask.innerText = value;
-        taskContent.appendChild(titleTask);
+    let value = input.value;
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
 
-        const content =
-            `
-        <i class="fas fa-trash-alt trashIcon icon"></i>
-        `
+    /* task.innerHTML = content; */
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
 
-        /* task.innerHTML = content; */
-        task.appendChild(taskContent);
+    list.appendChild(task);
+    input.value = "";
+}
 
-        list.appendChild(task);
-        input.value = "";
-    }
-
-    btn.addEventListener("click", creteTask)
-
-    const checkComplete = () => {
-        const i = document.createElement('i');
-        i.classList.add('far', 'fa-check-square', 'icon');
-        i.addEventListener("click", completeTask);
-        return i
-    }
-
-    const completeTask = (event) => {
-        const element = event.target;
-        element.classList.toggle('fas');
-        element.classList.toggle('complete')
-        element.classList.toggle('far');
-    }
-
-})();
+btn.addEventListener("click", creteTask)
